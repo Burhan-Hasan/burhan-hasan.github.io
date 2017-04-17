5b6701d0379516a28c8b75813dbfeda1e9908512
Burhan-Hasan <burhan.hasan.dev@gmail.com>
2017-04-17 21:48:07 +0400

    -

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
-var x = 90;
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4)
+        return;
+    data = JSON.parse(xhr.responseText);
+    console.log(data);
+};
+xhr.send();
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,1 +1,9 @@
-var x=90;
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4) return;
+    data=JSON.parse(xhr.responseText);
+    console.log(data);
+}
+xhr.send();
