54c10ea85ca77aadb258986598b80421e5a04d51
Burhan-Hasan <burhan.hasan.dev@gmail.com>
2017-04-18 11:28:53 +0400

    Added simple node js server

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,20 @@
-function GetSkills() {
-    var docFragment = document.createDocumentFragment();
-}
+var containers = document.getElementsByClassName('col-50');
 var Handlebars;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.send();
+var _data = null;
+xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4)
+        return;
+    _data = JSON.parse(xhr.responseText);
+    GenerateHTML();
+};
 var skills = Handlebars.compile(document.getElementById('skills').innerHTML);
 Handlebars.registerHelper('isActive', function (value, index) {
     return index <= value ? 'active' : '';
 });
-var containers = document.getElementsByClassName('col-50');
-//SKILLS
-containers[0].insertAdjacentHTML('afterBegin', skills(window['data']));
-function dataLoaded() {
-    debugger;
+function GenerateHTML() {
+    //SKILLS
+    containers[0].insertAdjacentHTML('afterBegin', skills(_data));
 }
